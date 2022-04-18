6f69237b9c89805da302d3416be55e1654100162
Adeleye-Temiloluwa <temiloluwagithub@gmail.com>
2022-04-18 13:41:48 +0100

    curly braces error fixed

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,15 +2,12 @@ const BlogList = ({blogs, title}) => {
 
     return ( 
         <div className="blog-list">
-            <h2>{title}</h2>
-            {blogs.map((blog) => {
-                <div className="blog-preview" key={blog.id}>
-                    <div>
-                        <h3>{blog.body}</h3>
-                        <p>{blog.author}</p>
-                    </div>
-                </div>
-            })}
+            {/* <h2>{title}</h2> */}
+            {blogs.map((blog) => 
+            (<div className="blog-preview">
+                <h2>{blog.title}</h2>
+                <h3>{blog.author}</h3>
+                </div>))}
         </div>
      );
 }
